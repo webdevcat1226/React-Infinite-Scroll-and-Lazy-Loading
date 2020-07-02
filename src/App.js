330cd23ef744ef3e212ca46c8598d0435917738e
leosatan-fairy <tikhonovich0220@gmail.com>
2020-07-02 18:14:18 +1000

    feat: adding infinite scrolling and image lazy-loading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useEffect, useReducer, useRef } from "react";
 
-import { useFetch, useInfiniteScroll, useLazyLoading } from './customHooks'
-import './index.css';
+import { useFetch, useInfiniteScroll, useLazyLoading } from "./customHooks";
+import "./index.css";
 
-function App() {
+function App () {
   const imgReducer = (state, action) => {
     switch (action.type) {
-      case 'STACK_IMAGES':
-        return { ...state, images: state.images.concat(action.images) }
-      case 'FETCHING_IMAGES':
-        return { ...state, fetching: action.fetching }
+      case "STACK_IMAGES":
+        return { ...state, images: state.images.concat(action.images) };
+      case "FETCHING_IMAGES":
+        return { ...state, fetching: action.fetching };
       default:
         return state;
     }
-  }
+  };
 
   const pageReducer = (state, action) => {
     switch (action.type) {
-      case 'ADVANCE_PAGE':
-        return { ...state, page: state.page + 1 }
+      case "ADVANCE_PAGE":
+        return { ...state, page: state.page + 1 };
       default:
         return state;
     }
-  }
+  };
 
-  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 })
-  const [imgData, imgDispatch] = useReducer(imgReducer, { images: [], fetching: true, })
+  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 });
+  const [imgData, imgDispatch] = useReducer(imgReducer, { images: [], fetching: true });
+
+  useEffect(() => {
+    imgDispatch({ type: "FETCHING_IMAGES", fetching: true });
+    fetch("https://picsum.photos/v2/list?page=0&limit=10")
+      .then(data => data.json())
+      .then(images => {
+        imgDispatch({ type: "STACK_IMAGES", images });
+        imgDispatch({ type: "FETCHING_IMAGES", fetching: false });
+      })
+      .catch(e => {
+        // handle error
+        imgDispatch({ type: "FETCHING_IMAGES", fetching: false });
+        return e;
+      });
+  }, [imgDispatch]);
 
   let bottomBoundaryRef = useRef(null);
   useFetch(pager, imgDispatch);
-  useLazyLoading('.card-img-top', imgData.images)
+  useLazyLoading(".card-img-top", imgData.images);
   useInfiniteScroll(bottomBoundaryRef, pagerDispatch);
 
   return (
@@ -45,7 +60,7 @@ function App() {
       <div id='images' className="container">
         <div className="row">
           {imgData.images.map((image, index) => {
-            const { author, download_url } = image
+            const { author, download_url } = image;
             return (
               <div key={index} className="card">
                 <div className="card-body ">
@@ -53,14 +68,14 @@ function App() {
                     alt={author}
                     data-src={download_url}
                     className="card-img-top"
-                    src={'https://picsum.photos/id/870/300/300?grayscale&blur=2'}
+                    src={"https://picsum.photos/id/870/300/300?grayscale&blur=2"}
                   />
                 </div>
                 <div className="card-footer">
                   <p className="card-text text-center text-capitalize text-primary">Shot by: {author}</p>
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </div>
@@ -70,7 +85,7 @@ function App() {
           <p className="m-0 text-white">Getting images</p>
         </div>
       )}
-      <div id='page-bottom-boundary' style={{ border: '1px solid red' }} ref={bottomBoundaryRef}></div>
+      <div id='page-bottom-boundary' style={{ border: "1px solid red" }} ref={bottomBoundaryRef}></div>
     </div>
   );
 }
